683495694bd90bebdd40294cd78c6544b856d45f
krisior <krzysiek.wilnicki@gmail.com>
2024-05-09 18:50:17 +0200

    [k] initial commit

---
+++ src/main/java/org/window/Simulation.java
@@ -0,0 +1,5 @@
+package org.window;
+
+public class Simulation {
+
+}
---
+++ src/test/java/testSimulation.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class testSimulation {
+
+    @Test
+    public void test() {
+
+    }
+}
